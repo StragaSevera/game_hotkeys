b186cc7a5d769d6e6876c9f63fe6a93a197916be
StragaSevera <ought@yandex.ru>
2021-06-30 10:16:29 +0300

    Fix stop action

--- src/main/kotlin/action/StopAction.kt
+++ src/main/kotlin/action/StopAction.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.delay
 
 class StopAction : Action() {
     override val loopTime: Long = 500
-    override val keys = listOf("NUMPAD3")
+    override val keys = listOf("NUMPAD5", "CLEAR")
 
     override suspend fun action() {
         while (true) {
